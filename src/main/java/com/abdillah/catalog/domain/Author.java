f05602921a903486941266e3c9eb3ba9e6fa22c6
achmdabdillah <achmdabdil9@gmail.com>
2023-06-02 07:09:40 +0700

    create Author table spec

--- src/main/java/com/abdillah/catalog/domain/Author.java
+++ src/main/java/com/abdillah/catalog/domain/Author.java
@@ -1,14 +1,31 @@
 package com.abdillah.catalog.domain;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "author")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "author_name", nullable = false)
     private String name;
 
-    private Long birthDate;
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
 }
