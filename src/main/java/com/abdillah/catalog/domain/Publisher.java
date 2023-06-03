e9d7f5919153024cafc2015f91fc9317bfb78e4d
achmdabdillah <achmdabdil9@gmail.com>
2023-06-03 09:43:03 +0700

    create one to many relation from  Publisher to Book

---
+++ src/main/java/com/abdillah/catalog/domain/Publisher.java
@@ -0,0 +1,35 @@
+package com.abdillah.catalog.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "publisher")
+public class Publisher implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "company_name", nullable = false)
+    private String companyName;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany(mappedBy = "publisher")
+    private List<Book> books;
+}
