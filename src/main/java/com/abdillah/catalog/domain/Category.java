207a0e050adceeb58510c5acec4ce9cc558308f2
achmdabdillah <achmdabdil9@gmail.com>
2023-06-03 10:15:12 +0700

    create many to many relation from  Category to Book

---
+++ src/main/java/com/abdillah/catalog/domain/Category.java
@@ -0,0 +1,32 @@
+package com.abdillah.catalog.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = -5438873451L;
+
+    @Id
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Book> books;
+}
